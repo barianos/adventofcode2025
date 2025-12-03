34fd775088bb372474c814c9082c52092e535e27
Anastasios Barianos <barianos@outlook.com>
2025-12-03 21:40:17 +0200

    day 3 part one

--- day03/day3.js
+++ day03/day3.js
@@ -1,15 +1,33 @@
 // Prepare input
 const fs = require('fs');
-const input = fs.readFileSync('testinput.txt', 'utf8').trim();
-// const input = fs.readFileSync('input.txt', 'utf8').trim();
-const lines = input.split(',');
+// const input = fs.readFileSync('testinput.txt', 'utf8').trim();
+const input = fs.readFileSync('input.txt', 'utf8').trim();
+const lines = input.split('\n');
 
 
 // Define needed data
-
+let joltage = 0;
 
 // Figure out the answer
-
+for (const line of lines) {
+    const individuals = line.trim("\r").split("");
+    let max = 0;
+    let secondMax = 0;
+    let maxPos = 0;
+    for(let i=0; i< individuals.length -1; i++){
+        if(individuals[i]>max){ 
+            max = Number(individuals[i]);
+            maxPos=i;
+        }
+    }
+    for(let i=maxPos + 1; i< individuals.length; i++){
+        if(individuals[i]>secondMax){ 
+            secondMax = Number(individuals[i]);
+        }
+    }
+    joltage+= (max*10) + secondMax;
+}
 
 
 // Print the Solution
+console.log(joltage);
