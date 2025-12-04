a8f853db8c84e19cb06ed4c0018516f423747fe2
Anastasios Barianos <barianos@outlook.com>
2025-12-04 23:32:38 +0200

    setup day 4

---
+++ day04/day4.js
@@ -0,0 +1,19 @@
+// Prepare input
+const fs = require('fs');
+// const input = fs.readFileSync('testinput.txt', 'utf8').trim();
+const input = fs.readFileSync('input.txt', 'utf8').trim();
+const lines = input.split('\n');
+
+
+// Define needed data
+let solution1=0;
+let solution2=0;
+
+
+// Figure out the answer for part 1
+
+
+// Figure out the answer for part 2
+
+// Print the Solution
+console.log({joltage, bigJoltage});
