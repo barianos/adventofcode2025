d7e08154c4c26cb95b2338bda6138147b8a344a0
Anastasios Barianos <barianos@outlook.com>
2025-12-05 19:12:54 +0200

    setup day 5

---
+++ day05/day5.js
@@ -0,0 +1,16 @@
+// Prepare input
+const fs = require('fs');
+const input = fs.readFileSync('testinput.txt', 'utf8').trim();
+// const input = fs.readFileSync('input.txt', 'utf8').trim();
+const lines = input.split('\n');
+
+
+// Define needed data
+let solution1, solution2;
+
+// Figure out the answer for part 1
+
+// Figure out the answer for part 2
+
+// Print the Solution
+console.log({solution1, solution2});
