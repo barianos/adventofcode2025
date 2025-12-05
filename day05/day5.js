1059e61b8bc572fd4c67d07d5c55dee66aee9521
Anastasios Barianos <barianos@outlook.com>
2025-12-05 20:00:18 +0200

    day5 part 1 solution

--- day05/day5.js
+++ day05/day5.js
@@ -1,16 +1,46 @@
 // Prepare input
 const fs = require('fs');
-const input = fs.readFileSync('testinput.txt', 'utf8').trim();
-// const input = fs.readFileSync('input.txt', 'utf8').trim();
-const lines = input.split('\n');
+// const input = fs.readFileSync('testinput.txt', 'utf8').trim();
+const input = fs.readFileSync('input.txt', 'utf8').trim();
+const lines = input.split('\n').map(line => line.trim());
 
 
 // Define needed data
-let solution1, solution2;
+let solution1 = 0
+let solution2 = 0;
 
+// Find the index of the empty line
+const emptyLineIndex = lines.indexOf('');
+
+// Split into two arrays
+const ranges = lines.slice(0, emptyLineIndex);
+const items = lines.slice(emptyLineIndex + 1);
+
+console.log({ranges, items});
 // Figure out the answer for part 1
+for(item of items){
+    let found = findNumberInRanges(Number(item));
+    if(found){
+        solution1++;
+    }
+}
 
 // Figure out the answer for part 2
 
 // Print the Solution
 console.log({solution1, solution2});
+
+// Helper Functions
+function findNumberInRanges(num){
+    for(range of ranges){
+        let akra = range.split('-');
+        console.log({num, akra});
+        
+        if(num >= Number(akra[0]) && num <= Number(akra[1])){
+            return true;
+
+        }
+    }
+    return false;
+
+}
