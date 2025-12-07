c512d554a001b3aad04e4c2352bddb3f17dec90f
Anastasios Barianos <barianos@outlook.com>
2025-12-07 19:57:35 +0200

    day 6 part 1

--- day06/day6.js
+++ day06/day6.js
@@ -1,15 +1,32 @@
 // Prepare input
 const fs = require('fs');
-const input = fs.readFileSync('testinput.txt', 'utf8').trim();
-// const input = fs.readFileSync('input.txt', 'utf8').trim();
-const lines = input.split('\n').map(line => line.trim());
-
+// const input = fs.readFileSync('testinput.txt', 'utf8').trim();
+const input = fs.readFileSync('input.txt', 'utf8').trim();
+// const lines = input.split('\n').map(line => line.trim());
+const lines = input.split('\n');
 
 // Define needed data
+const rows = lines.map(line => line.match(/\S+/g));
+const columns = rows[0].map((_, colIndex) =>
+    rows.map(row => row[colIndex])
+);
+const ops = {
+  "*": (a, b) => a * b,
+  "+": (a, b) => a + b
+};
+
 let solution1 = 0
 let solution2 = 0;
 
 // Figure out the answer for part 1
+for (const col of columns) {
+    const opSymbol = col[col.length - 1];
+    const nums = col.slice(0, -1).map(Number);
+    const op = ops[opSymbol];
+    const result = nums.reduce(op);
+    solution1 += result;
+
+}
 
 // Figure out the answer for part 2
 
